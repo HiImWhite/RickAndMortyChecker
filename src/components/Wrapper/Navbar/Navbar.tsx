f0b3f2d410558427ba853818142b202f1806c0f5
HiImWhite <wojttasek1999@gmail.com>
2023-03-23 18:55:44 +0100

    Update Navbar.tsx

--- src/components/Wrapper/Navbar/Navbar.tsx
+++ src/components/Wrapper/Navbar/Navbar.tsx
@@ -1,9 +1,28 @@
-import { AppBar, Toolbar } from '@mui/material';
+import {
+  AppBar,
+  Button,
+  Toolbar,
+  Stack,
+  IconButton,
+  Typography,
+} from '@mui/material';
+import ScienceRoundedIcon from '@mui/icons-material/ScienceRounded';
 
 const Navbar = () => {
   return (
     <AppBar position='static'>
-      <Toolbar></Toolbar>
+      <Toolbar>
+        <IconButton size='large' aria-label='logo'>
+          <ScienceRoundedIcon />
+        </IconButton>
+        <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+          Rick and Morty APP
+        </Typography>
+        <Stack direction='row' spacing={2}>
+          <Button color='inherit'>R&M World</Button>
+          <Button color='inherit'>Create Character</Button>
+        </Stack>
+      </Toolbar>
     </AppBar>
   );
 };

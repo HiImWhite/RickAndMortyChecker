506387544bb74a24ece9fa913e8a6d7fbf8a2c26
HiImWhite <wojttasek1999@gmail.com>
2023-03-23 21:11:54 +0100

    Views

---
+++ src/views/CreateCharacter.tsx
@@ -0,0 +1,11 @@
+import Wrapper from '../components/Wrapper/Wrapper';
+
+const CreateCharacter = () => {
+  return (
+    <>
+      <Wrapper></Wrapper>
+    </>
+  );
+};
+
+export default CreateCharacter;
---
+++ src/views/LandingPage.tsx
@@ -0,0 +1,13 @@
+import MainContent from '../components/MainContent/MainContent';
+import Wrapper from '../components/Wrapper/Wrapper';
+
+const LandingPage = () => {
+  return (
+    <>
+      <Wrapper />
+      <MainContent />
+    </>
+  );
+};
+
+export default LandingPage;

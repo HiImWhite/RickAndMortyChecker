39941871a9c2d90aeaca8083c4b78b6823ffb5b5
HiImWhite <wojttasek1999@gmail.com>
2023-03-29 21:08:03 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,12 @@ import Wrapper from './components/Wrapper/Wrapper';
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Wrapper />}>
-        <Route path='/' element={<LandingPage />} />
-        <Route path='/random-character' element={<RandomCharacter />} />
+      <Route path='/RickAndMortyChecker' element={<Wrapper />}>
+        <Route path='/RickAndMortyChecker' element={<LandingPage />} />
+        <Route
+          path='/RickAndMortyChecker/random-character'
+          element={<RandomCharacter />}
+        />
         <Route path='*' element={<NotFound />} />
       </Route>
     </Routes>
